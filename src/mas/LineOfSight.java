67444548b3d0e41cba2871bf930a37c855fade6e
koenleunen <koenleunen@Koens-MacBook-Pro.local>
2015-05-20 13:13:49 +0200

    aantal contractnet message types toegevoegd

--- src/mas/LineOfSight.java
+++ src/mas/LineOfSight.java
@@ -7,4 +7,6 @@ public abstract class LineOfSight {
 	abstract boolean canComm(FireFighter ff1, FireFighter ff2);
 	abstract double getVisionRadius(); // how far can I see
 
+	// TODO: ik denk dat contract net het beste werkt tov andere in de situatie
+	// dat canSee beperkt is, maar canComm niet (dus communicatie altijd mogelijk, maar geen weet van al het vuur)
 }
